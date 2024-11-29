a6ea405750420c209d8244abe60fa49cb96d8ba5
Mário Gonçalves da Silva <mariskovisk@gmail.com>
2024-11-29 11:40:14 -0300

    front-end test adopet website

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
+describe('templat spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+      cy.visit('https://adopet-tau.vercel.app/');
+      cy.contains('a', 'Cadastrar').click();
+      cy.get('input[name="name"]').type('KS');
+      cy.get('input[name="email"]').type('ks@email.com');
+      cy.get('input[name="password"]').type('Senha123');
+      cy.get('input[name="confirm_password"]').type('Senha123');
+      cy.contains('button', 'Cadastrar').click();
+   })
 })
