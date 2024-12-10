0730264445524c72eb94fc7f118ca81e8df207c7
Mário Gonçalves da Silva <mariskovisk@gmail.com>
2024-12-10 11:52:59 -0300

    refactoring tests

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,11 +1,9 @@
 describe('Página de cadastro', () => {
-   it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
-     cy.visit('https://adopet-frontend-cypress.vercel.app/');
-     cy.get('[data-test="register-button"]').click();
-     cy.get('[data-test="input-name"]').type('Machado de Assis');
-     cy.get('[data-test="input-email"]').type('machadodeassis@email.com');
-     cy.get('[data-test="input-password"]').type('Senha123');
-     cy.get('[data-test="input-confirm-password"]').type('Senha123');
-     cy.get('[data-test="submit-button"]').click();
-   })
- })
+  beforeEach(() =>{
+      cy.visit('https://adopet-frontend-iota.vercel.app/');
+      cy.get('[data-test="register-button"]').click();
+  })   
+      it('Deve preencher os campos do cadastro corretamente e clicar no botão cadastrar', () => {
+          cy.cadastrar('Marjorie Anoquevem','marjorie@email.com', 'Senha123')  
+  })
+})
