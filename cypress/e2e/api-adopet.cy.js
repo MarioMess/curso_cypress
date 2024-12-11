2badee15c972bd3c1fc70c3f72bebcb4e8299c23
Mário Gonçalves da Silva <mariskovisk@gmail.com>
2024-12-11 13:51:24 -0300

    test incorrect login with stubs

---
+++ cypress/e2e/api-adopet.cy.js
@@ -0,0 +1,18 @@
+
+describe('Api Adopet', () => {
+            const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNDQ1MWM2Yi01NDMxLTRiYmUtODg1MS01NmI4NTYzM2YzNjAiLCJhZG9wdGVyTmFtZSI6IkNhbWlsYSIsImlhdCI6MTcwODE4MTgwOSwiZXhwIjoxNzA4NDQxMDA5fQ.RtJOWoIQCVNrT1AQw17n4pYo4N2AAqBOokqv_XJ22to
+            `
+                    it('Mensagens', () => {
+                        cy.request({
+                            method: 'GET' ,
+                            url: 'https://adopetapi-pgks.onrender.com/mensagem/14451c6b-5431-4bbe-8851-56b85633f360',
+                            headers: { authorization }
+                           
+                        }).then((res) => {
+                            expect(res.status).to.be.equal(200)
+                            expect(res.body).is.not.empty
+                            expect(res.body).to.have.property('msg')
+                                    
+                        })
+                    })
+                })
